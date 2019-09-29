e4dbda9b9d0f4704ba1407c412eeb55c82685edd
jassiG <55924890+jassiG@users.noreply.github.com>
2019-09-29 13:03:30 +0530

    Update FinalCode.py

--- FinalCode.py
+++ FinalCode.py
@@ -207,7 +207,7 @@ if __name__ == "__main__":
              inp=''
              query=''
         elif 'google search' in query:
-            query=query.replace("search","")
+            query=query.replace("Google search","")
             chrome_path = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
             for url in search(query, tld="co.in", num=1, stop = 1, pause = 2):
                  webbrowser.open("https://google.com/search?q=%s" % query)
